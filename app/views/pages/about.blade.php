f68601e7db36b16e2c44ad768769337deee3aa6b
Bobby Cahill <bobby@bobbycahill.com>
2014-10-31 00:35:14 -0400

    Update documentation pages
    Updated the features list on the home page and also edited and added
    some quick formatting to the about/contribute pages to make them look a
    bit nicer.

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -2,65 +2,120 @@
 
 @section('bodyContent')
 
-<h1>What is Projectify</h1>
 
-<p>You're busy.  You have commitments.  They need to get done.  But you can't do them all now.  In fact, maybe you can only do 3 of them now.</p>
+<div class="well">
 
-<p>Why waste energy thinking about the tasks you can't do now?  Focus on the doable tasks now and let Projectify keep track of the rest until you can do them!</p>
+    <h1>What is Projectify</h1>
 
-<h2>Open Source</h2>
+    <p>You're busy.  You have commitments.  They need to get done.  But you
+    can't do them all now.  In fact, maybe you can only do 3 of them now.</p>
 
-<p>Projectify is open-source, which means the code is freely available.  The community can see the code and contribute.  If you like this style of task management and want to help, check out the <a href="/contribute">Contribute</a> page to find out how you can help</p>
+    <p>Why waste energy thinking about the tasks you can't do now?  Focus on the
+    doable tasks now and let Projectify keep track of the rest until you can do
+    them!</p>
 
-<h2>Reasons a task can't be completed</h2>
+</div>
 
-<p>When you can't complete a task, there are generally 3 reasons</p>
 
-<ul><li>Context</li>
-<li>Complexity</li>
-<li>Roadblocks</li>
-</ul>
+<div class="well">
 
-<h3>Context</h3>
+    <h2>Open Source</h2>
 
-<p>If your task requires you to be in a certain place or have access to certain equipment.  For example, if your task is "Mow the Lawn", you could apply the context "home" while adding it, since it can only be done at home.  If you're out running errands, you may be in the "car" context.  Since you're not in the right context to complete this task, Projectify hides it for now.</p>
+    <p>Projectify is open-source, which means the code is freely available.  The
+    community can see the code and contribute.  If you like this style of task
+    management and want to help, check out the
+    <a href="/contribute">Contribute</a> page to find out how you can help</p>
 
-<h3>Complexity</h3>
+</div>
 
-<p>Maybe you started working on a task, but weren't able to complete it yet.  You may be inclined to mark the task with a status, like "in progress" or 15% Complete.  But these arbitrary labels are a bit abstract and don't do a great job of illustrating your progress.  If you start accumulating a lot of tasks with these partially complete labels, it can make a mess, and now you're back to using mental energy every time you look at your list.</p>
 
-<p>What do yo do with a task like this?  <strong>Projectify it!</strong></p>
+<div class="well">
 
-<p>Click the Projectify icon next to the task, and you will have the option to turn a Task into a Project by adding subtasks.</p>
+    <h2>Reasons a task can't be completed</h2>
 
-<h3>Roadblocks</h3>
+    <p>When you can't complete a task in life, there are generally 3 reasons</p>
 
-<p>What if a task can't be done for external reasons that have nothing to do with you?  Or if a task is dependent upon completion of another task?  We call these <strong>Roadblocks</strong>, and they generally can be broken down into three types:</p>
+</div>
 
-<ol>
-<li>Waiting on Task</li>
-<li>Waiting on Contact</li>
-<li>Waiting on Event</li>
-</ol>
 
-<p><em>Note: roadblocks haven't been implemented yet, but here's the current plan for how they will work once they are.</em></p>
+<div class="well">
 
-<h4>Waiting on Task</h4>
+    <h3>Reason 1: Context</h3>
 
-<p>When you add this Roadblock, you can reference another task or project.  This task will remain hidden until the other task is completed.</p>
+    <p>If your task requires you to be in a certain place or have access to
+    certain equipment.  For example, if your task is "Mow the Lawn", you could
+    apply the context "home" while adding it, since it can only be done at home.  
+    If you're out running errands, you may be in the "car" context.  Since
+    you're not in the right context to complete this task, Projectify hides it
+    for now.</p>
 
-<h4>Waiting on Contact</h4>
+</div>
 
-<p>When you add this roadblock, you will be able to associate a contact and a date/time when you'd like a reminder to follow up with that contact.</p>
 
-<h4>Waiting on Event</h4>
+<div class="well">
 
-<p>Adding this roadblock allows you to add a date/time when the task will become visible again.</p>
+    <h3>Reason 2: Complexity</h3>
 
-<h2>Get Stuff Done</h2>
+    <p>Maybe you started working on a task, but weren't able to complete it yet.
+    You may be inclined to mark the task with a status, like "in progress" or 
+    15% Complete.  But these arbitrary labels are a bit abstract and don't do a 
+    great job of illustrating your progress.  If you start accumulating a lot of 
+    tasks with these partially complete labels, it can make a mess, and now 
+    you're back to using mental energy every time you look at your list.</p>
 
-<p>Using the tools explained above, you should be able to focus on only the tasks you can do now without being distracted by all of the other tasks you are responsible for.  This should make it much easier to prioritize these tasks on the fly based on importance or personal preference.</p>
+    <p>What do yo do with a task like this?  <strong>Projectify it!</strong></p>
 
-<p>With the peace of mind that comes from knowing you will be reminded of doable tasks at an appropriate time, you will have the confidence to focus on the things that are important right now andstop worrying that you might forget something.</p>
+    <p>Click the Projectify icon next to the task, and you will have the option 
+    to turn a Task into a Project by adding subtasks.</p>
+
+</div>
+
+
+<div class="well">
+
+    <h3>Reason 3: Roadblocks</h3>
+
+    <p>What if a task can't be done for external reasons that have nothing to do 
+    with you?  Or if a task is dependent upon completion of another task?  We 
+    call these <strong>Roadblocks</strong>, and they generally can be broken 
+    down into three types:</p>
+
+    <p><em>Note: roadblocks haven't been implemented yet, but here's the current
+        plan for how they will work once they are.</em></p>
+
+    <h4>Roadblock type 1: Waiting on Task</h4>
+
+    <p>When you add this Roadblock, you can reference another task or project.
+    This task will remain hidden until the other task is completed.</p>
+
+    <h4>Roadblock type 2: Waiting on Contact</h4>
+
+    <p>When you add this roadblock, you will be able to associate a contact and
+    a date/time when you'd like a reminder to follow up with that contact.</p>
+
+    <h4>Roadblock type 3: Waiting on Event</h4>
+
+    <p>Adding this roadblock allows you to add a date/time when the task will
+    become visible again.</p>
+
+</div>
+
+
+<div class="well">
+
+    <h2>Get Stuff Done</h2>
+
+    <p>Using the tools explained above, you should be able to focus on only the
+    tasks you can do now without being distracted by all of the other tasks you
+    are responsible for.  This should make it much easier to prioritize these
+    tasks on the fly based on importance or personal preference.</p>
+
+    <p>With the peace of mind that comes from knowing you will be reminded of
+    doable tasks at an appropriate time, you will have the confidence to focus
+    on the things that are important right now andstop worrying that you might
+    forget something.</p>
+
+</div>
+
 
 @stop
--- app/views/pages/contribute.blade.php
+++ app/views/pages/contribute.blade.php
@@ -2,27 +2,55 @@
 
 @section('bodyContent')
 
-<h1>Contributing</h1>
+<div class="well">
 
-<p>Do you love Projectify and want to help us make it even better?  We would love your help!  Here are some things you can do:</p>
+    <h1>Contributing</h1>
 
-<h2>Report Bugs and Send Feedback</h2>
+    <p>Do you love Projectify and want to help us make it even better?  We would
+    love your help!  Here are some things you can do:</p>
 
-<p>For now, you can report bugs by emailing <a href="mailto:bobby@proj.ectify.com">Projectify Support</a>.</p>
+</div>
 
-<h2>Donate (not set up yet)</h2>
 
-<p>We haven't set up a means for donating, but if you're interested, send an email to <a href="mailto:bobby@proj.ectify.com">Projectify Support</a> and we will contact you when it's ready.</p>
+<div class="well">
 
-<h2>Developers</h2>
+    <h2>Report Bugs and Send Feedback</h2>
 
-<p>We are looking for help with general development, but here are some specific things we'd like some help with:</p>
+    <p>For now, you can report bugs by emailing
+    <a href="mailto:bobby@proj.ectify.com">Projectify Support</a>.</p>
 
-<ul><li>Implementing Testing</li>
-<li>Layout/Front End</li>
-<li>Graphic Design</li>
-</ul>
+</div>
 
-<p>The code is available on <a href="https://github.com/BBBThunda/projectify">GitHub</a> with instructions that should help anyone get a test server running on your machine so you can play with the code and submit pull requests.</p>
+
+<div class="well">
+
+    <h2>Donate (not set up yet)</h2>
+
+    <p>We haven't yet set up a means for donating, but if you're interested,
+    send an email to
+    <a href="mailto:bobby@proj.ectify.com">Projectify Support</a> and we will
+    contact you when it's ready.</p>
+
+</div>
+
+
+<div class="well">
+
+    <h2>Developers</h2>
+
+    <p>We are looking for help with general development, but here are some
+    specific areas in which we could use some help:</p>
+
+    <ul><li>Implementing Testing/CI</li>
+        <li>Design/Layout/Front End</li>
+        <li>Simple Graphic Design</li>
+    </ul>
+
+    <p>The code is available on
+    <a href="https://github.com/BBBThunda/projectify">GitHub</a> with
+    instructions that should help anyone get a test server running on your
+    machine so you can play with the code and submit pull requests.</p>
+
+</div>
 
 @stop
